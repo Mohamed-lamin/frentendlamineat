66c1edb993d722ba85aeef430ae7964ed308ef0a
Damin <daminhaidar08@gmail.com>
2023-01-20 13:26:03 +0100

    better url

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const url = "https://lamineatbackend-lamineat.onrender.com/"
+const url = "https://lamineatbackend-lamineat.onrender.com/plats"
 
 export const fetchPlats = () => axios.get(url)
 export const createPlat = newPlatPost => axios.post(url, newPlatPost)
